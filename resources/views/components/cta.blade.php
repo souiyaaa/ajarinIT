06b633edb9cbc70d3d5f04107eecaf57bda0c6f0
souiyaaa <153163259+souiyaaa@users.noreply.github.com>
2025-07-15 19:05:27 +0700

    Fix: added missing button links

--- resources/views/components/cta.blade.php
+++ resources/views/components/cta.blade.php
@@ -1,5 +1,7 @@
 <div>
-    <a href="#" class="bg-[#19A4D3] inline-flex rounded">
+    {{-- test --}}
+    {{-- link to whatsapp --}}
+    <a href="https://wa.link/np14hw" class="bg-[#19A4D3] inline-flex rounded">
         <span class="text-white font-bold leading-none px-6 py-3">Daftar Sekarang</span>
     </a>
 </div>
