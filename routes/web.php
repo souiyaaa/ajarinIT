eae9431a421840d3c287836c2be5ee0dce16af2e
souiyaaa <153163259+souiyaaa@users.noreply.github.com>
2025-07-16 10:43:03 +0700

    Install spatie/laravel-sitemap and add sitemap route fixed router

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\Tags\Url;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/sitemap.xml', function () {
     $sitemap = Sitemap::create()
