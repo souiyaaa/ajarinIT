02b42ad1ec123b292aa93c698b9c4d09515460b5
souiyaaa <153163259+souiyaaa@users.noreply.github.com>
2025-07-16 10:36:03 +0700

    Add sitemap.xml route using Spatie package

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/sitemap.xml', function () {
+    $sitemap = Sitemap::create()
+        ->add(
+            Url::create('/')
+                ->setLastModificationDate(now())
+                ->setPriority(1.0)
+        );
+
+    return $sitemap->toResponse(request());
 });
